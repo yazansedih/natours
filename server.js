b9ba2372c661a74cca8d5b24253749afe8b2279b
yazansedih <yazansedih@gmail.com>
2024-06-08 11:56:22 +0300

    video 84 is done.

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 // Load environment variables from config.env file
 dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful! 😁');
+  })
+  .catch((err) => {
+    console.error('Error connecting to DB:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
