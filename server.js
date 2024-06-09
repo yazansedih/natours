cdc5c9db13686384d9cd111023c940a4d80db68d
yazansedih <yazansedih@gmail.com>
2024-06-09 15:47:10 +0300

    87 video is done.

--- server.js
+++ server.js
@@ -16,10 +16,51 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('DB connection successful! 😁');
+    console.log('DB connection successful!😁');
   })
   .catch((err) => {
     console.error('Error connecting to DB:', err);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forset Hiker',
+  rating: 4.7,
+  price: 500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥: ', err);
   });
 
 const port = process.env.PORT || 3000;
